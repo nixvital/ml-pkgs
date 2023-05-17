70be101981bee9d44712a9fafb3cca94481327af
Break Yang <breakds@gmail.com>
2023-05-17 10:42:13 -0700

    Add back dm-control 1.0.9

--- overlays/simulators.nix
+++ overlays/simulators.nix
@@ -38,12 +38,12 @@ final: prev: {
       # Mujoco (Official Python Binding) and friends
       mujoco-pybind = python-final.callPackage ../pkgs/mujoco-pybind {};
       mujoco-pybind-231 = python-final.callPackage ../pkgs/mujoco-pybind/2.3.1.nix {};
-      mujoco-pybind-234 = python-final.callPackage ../pkgs/mujoco-pybind/2.3.4.nix {};
       mujoco-menagerie = python-final.callPackage ../pkgs/mujoco-menagerie {};
       dm-tree = python-final.callPackage ../pkgs/deepmind/dm-tree {};
       dm-env = python-final.callPackage ../pkgs/deepmind/dm-env {};
       labmaze = python-final.callPackage ../pkgs/deepmind/labmaze {};
       dm-control = python-final.callPackage ../pkgs/deepmind/dm-control {};
+      dm-control-109 = python-final.callPackage ../pkgs/deepmind/dm-control/1.0.9.nix {};
       python-fcl = python-final.callPackage ../pkgs/python-fcl {};
     })
   ];
---
+++ pkgs/deepmind/dm-control/1.0.9.nix
@@ -0,0 +1,73 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, pyparsing
+# Runtime Requirements
+, absl-py
+, dm-env
+, dm-tree
+, glfw
+, h5py
+, labmaze
+, lxml
+, mujoco-pybind-231
+, numpy
+, pillow
+, protobuf
+, pyopengl
+, requests
+, scipy
+, tqdm
+}:
+
+buildPythonPackage rec {
+  pname = "dm_control";
+  version = "1.0.9";
+
+  src = fetchFromGitHub {
+    owner = "deepmind";
+    repo = "dm_control";
+    rev = version;
+    hash = "sha256-ExOFtthl3rSW8Z4wFa1LKIrZt3hnLNOiRM3v7wjEg8c=";
+  };
+
+  buildInputs = [
+    setuptools
+    pyparsing
+  ];
+
+  propagatedBuildInputs = [
+    absl-py
+    dm-env
+    dm-tree
+    glfw
+    h5py
+    labmaze
+    lxml
+    mujoco-pybind-231
+    numpy
+    pillow
+    protobuf
+    pyopengl
+    requests
+    scipy
+    tqdm
+  ];
+
+  pythonImportsCheck = [
+    "dm_control"
+  ];
+
+  doCheck = false;
+
+  meta = with lib; {
+    description = ''
+      DeepMind's software stack for physics-based simulation and
+      Reinforcement Learning environments, using MuJoCo.
+    '';
+    homepage = "https://github.com/deepmind/dm_control";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ breakds ];
+  };
+}
