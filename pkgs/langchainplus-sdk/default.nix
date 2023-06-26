26a3da0bc7eb2e3ab73cd24f98f3a465f383b3e9
Break Yang <breakds@gmail.com>
2023-06-25 23:07:40 -0700

    Build langchainplus-sdk from source

--- pkgs/langchainplus-sdk/default.nix
+++ pkgs/langchainplus-sdk/default.nix
@@ -1,26 +1,30 @@
 { lib
-, fetchurl
 , buildPythonPackage
+, fetchFromGitHub
+, poetry-core
 , pydantic
 , requests
 , tenacity
 }:
 
-# NOTE(breakds): This is not a complicated package and Ideally I should build it
-# from source. However, the source of this package uses a Makefile which
-# requires poetry command. See
-# https://discourse.nixos.org/t/question-of-packaging-python-package-with-poetry-and-makefile/29613
-let wheel = {
-      url = https://files.pythonhosted.org/packages/32/92/b4ce0f5c576233ccfdadce90031b64b9a12be5ed3ce9d555b28af34845bf/langchainplus_sdk-0.0.17-py3-none-any.whl;
-      sha256 = "1gq5zdzlbxg5w5n2svsvaghmshmlqgal6xnfj6b85c0bhpz7b5l9";
-    };
-
-in buildPythonPackage rec {
+buildPythonPackage rec {
   pname = "langchainplus-sdk";
   version = "0.0.17";
-  format = "wheel";
 
-  src = fetchurl wheel;
+  src = fetchFromGitHub {
+    owner = "langchain-ai";
+    repo = "langchainplus-sdk";
+    rev = "v${version}";
+    hash = "sha256-yeMNAsAcAU91qPwmv268ikZ7EHzW00c0nt1Zj8AJzgo=";
+  };
+
+  sourceRoot = "source/python";
+
+  format = "pyproject";
+
+  nativeBuildInputs = [
+    poetry-core
+  ];
 
   propagatedBuildInputs = [
     pydantic
@@ -28,16 +32,5 @@ in buildPythonPackage rec {
     tenacity
   ];
 
-  pythonImportsCheck = [
-    "langchainplus_sdk"
-  ];
-
-  meta = with lib; {
-    description = ''
-      Python client for interacting with the LangSmith platform.
-    '';
-    homepage = "";
-    license = licenses.mit;
-    maintainers = with maintainers; [ breakds ];
-  };
+  doCheck = false;
 }
