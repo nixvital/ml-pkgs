ebd11dd3711a54949a92051c120565daaa879362
Break Yang <breakds@gmail.com>
2023-06-25 17:59:04 -0700

    Fix build

--- overlays/misc.nix
+++ overlays/misc.nix
@@ -5,11 +5,6 @@ final: prev: {
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     (python-final: python-prev: {
       numerapi = python-final.callPackage ../pkgs/numerapi {};
-
-      # huggingface-transformers = python-final.callPackage ../bleeding/huggingface-transformers {
-      #   pytorch = python-final.pytorchWithCuda11;
-      # };
-
     })
   ];
 }
