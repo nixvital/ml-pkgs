bb0f568a58d9d82c1e6d2fb4035e023722da47d0
Break Yang <breakds@gmail.com>
2023-06-28 13:56:03 -0700

    Upgrade mujoco to 2.3.6

--- pkgs/mujoco-pybind/default.nix
+++ pkgs/mujoco-pybind/default.nix
@@ -12,22 +12,22 @@
 
 let wheels = {
       "x86_64-linux-python-3.9" = {
-        url = https://files.pythonhosted.org/packages/02/c7/bec8fce4bbe70e11d6e81f78f9c6413eaaa02952db2be9d5a1a0fd8f8c0d/mujoco-2.3.5-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl;
-        sha256 = "114x37bq1c3951zyynaw0zamh3q615bjb51a2xbwpgn4njj84f12";
+        url = https://files.pythonhosted.org/packages/48/4f/ab8bcb1eed1ab8d5866d31cf8cd48065cff28668623e7f67dc8addd7accb/mujoco-2.3.6-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl;
+        sha256 = "02384swcqmir6x84nr0ii735n6bnl8j34apz7bi93c6rhn7ksc4g";
       };
       "x86_64-linux-python-3.10" = {
-        url = https://files.pythonhosted.org/packages/7f/cd/7257c20302a51417ca970a6cdf77c8fd42ee5574b3563bb4ea98e75d3332/mujoco-2.3.5-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl;
-        sha256 = "181sn3rqyjfxx9mkhb5wdy1j1hvxbnvlhjx56wrdlks53x1iqjwg";
+        url = https://files.pythonhosted.org/packages/08/33/ec4a7b1073d9f3e5bebd9a19c3a70b02093c2926db8fb48d2aed59f30e31/mujoco-2.3.6-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl;
+        sha256 = "1f8g6j3wzd6wmr80hp2rlaf4crkf27zgsssg2x9a8r3mfwhjafpp";
       };
       "x86_64-linux-python-3.11" = {
-        url = https://files.pythonhosted.org/packages/ba/68/0975be108e0ddae4b28f2274cc33efc75db49ee7e6354433e519bc692ce6/mujoco-2.3.5-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl;
-        sha256 = "051xhhbby770dmnk8sj2irc1zix8hqkyy87qf7797a8pbv5qggrj";
+        url = https://files.pythonhosted.org/packages/25/60/4d05a28578e2cf021c72b46fda7420a2ffce6eb547ea443147f942661a6b/mujoco-2.3.6-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl;
+        sha256 = "1jhd3jla7q0m503y6gwgvd4nbvka9hzc057mffa1h9gql6zsrw0x";
       };
     };
 
 in buildPythonPackage rec {
   pname = "mujoco";
-  version = "2.3.5";
+  version = "2.3.6";
   format = "wheel";
 
   src = fetchurl wheels."${stdenv.system}-python-${python.pythonVersion}";
