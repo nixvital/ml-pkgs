7355c31525285d20d1011cc2bed190884160a60f
Break Yang <breakds@gmail.com>
2023-11-20 14:37:10 -0800

    Upgrade mujoco-pybind to 3.0.1

--- pkgs/mujoco-pybind/default.nix
+++ pkgs/mujoco-pybind/default.nix
@@ -12,22 +12,26 @@
 
 let wheels = {
       "x86_64-linux-python-3.9" = {
-        url = https://files.pythonhosted.org/packages/c2/ef/09e0a263487c26ddc76f5cdcf854bd161bd77888a4ec13fe560f1caf63f5/mujoco-3.0.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl;
-        sha256 = "16yxsm5s0kkcp0g6w1q770m38cqgi47k3fr3lyqgfy0pk5pcwg74";
+        url = https://files.pythonhosted.org/packages/56/9b/dc44edd747e12fbab8793746876cb792ba66683477661baaf93e4a3bdf37/mujoco-3.0.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl;
+        sha256 = "1ixk9dsw942snn5ssl15wk8g3071v8v4ja2md3siz27aga7qjaky";
       };
       "x86_64-linux-python-3.10" = {
-        url = https://files.pythonhosted.org/packages/ba/c3/ac38f009d419f3fd82402e23c367c545a104a272d1d64269e97de9b53c30/mujoco-3.0.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl;
-        sha256 = "0xih11mbydd2skjkwm5zzdl04z06n7x3j1hrw6hz4y28r1c7vs27";
+        url = https://files.pythonhosted.org/packages/7a/fa/d78b04b55f7cb1b2e66c2893ae9d847a426ed32e60658b6055af28f39eac/mujoco-3.0.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl;
+        sha256 = "151gim16ai1ifdnrf70k45h026qdgznzv041xfb47gzyb20f0k1z";
       };
       "x86_64-linux-python-3.11" = {
-        url = https://files.pythonhosted.org/packages/58/b6/d0f3c40437dc7962ed73c591f7b09e43d64b9b03786de72fdc62d74c3190/mujoco-3.0.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl;
-        sha256 = "08gs6xwyid7a5z2h3cd5lg7c7h60c1azjjf3yi9r2f3wysw8z81j";
+        url = https://files.pythonhosted.org/packages/47/99/102271e46fd00a15e7d47ecf7ebff2d9c33c9cdca484bf0dce60189decac/mujoco-3.0.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl;
+        sha256 = "0qgmmfvl3da9lc40gbmg9alh0h09121mdjb7y45vqkmx56bf2fd8";
+      };
+      "x86_64-linux-python-3.12" = {
+        url = https://files.pythonhosted.org/packages/44/a4/fac9ce45d933d71d484696a68c4634f32cbc65db74eb1e2e77ce327375f4/mujoco-3.0.1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl;
+        sha256 = "1mkipp8x080sfnydf8zrwfm49qv4zkhjx9yrad7in9b6y29myx4i";
       };
     };
 
 in buildPythonPackage rec {
   pname = "mujoco";
-  version = "3.0.0";
+  version = "3.0.1";
   format = "wheel";
 
   src = fetchurl wheels."${stdenv.system}-python-${python.pythonVersion}";
