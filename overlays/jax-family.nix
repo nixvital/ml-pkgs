c35b11647b89f7a1b7977383f97e066f29fda9f6
Break Yang <breakds@gmail.com>
2024-01-23 10:06:51 -0800

    Back to 23.05

--- overlays/jax-family.nix
+++ overlays/jax-family.nix
@@ -1,6 +1,13 @@
-final: prev: {
+final: prev:
+
+let cuda-118 = prev.cudaPackages_11_8;
+
+in {
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     (python-final: python-prev: {
+      # jaxlibWithCuda = python-prev.jaxlibWithCuda.override {
+      #   cudaPackages = cuda-118;
+      # };
       equinoxWithCuda11 = python-final.callPackage ../pkgs/equinox {};
     })
   ];
