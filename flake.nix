f9596d12c74707a07b8bd14813318fe57feb2303
Break Yang <breakds@gmail.com>
2024-05-23 11:52:50 -0700

    nixos-24.05 branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Provide extra Nix packages for Machine Learning and Data Science";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     utils.url = "github:numtide/flake-utils";
   };
