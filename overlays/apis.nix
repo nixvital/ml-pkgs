3a3e5aa4bcfa3713cd1473abd592405c0a985df6
Break Yang <breakds@gmail.com>
2024-08-29 15:21:31 +0800

    Upgrade a few aider dependencies

--- overlays/apis.nix
+++ overlays/apis.nix
@@ -1,6 +1,9 @@
 final: prev: {
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     (python-final: python-prev: {
+      inline-snapshot = python-final.callPackage ../bleeding/inline-snapshot {};
+      openai = python-final.callPackage ../bleeding/openai {};
+      litellm = python-final.callPackage ../bleeding/litellm {};
       wolframalpha = python-final.callPackage ../pkgs/apis/wolframalpha {};
     })
   ];
