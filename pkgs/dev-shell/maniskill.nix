a763611a320a4f34116866ef8395d5aa2b310e0f
Howard Nguyen-Huu <howardnguyenhuu@gmail.com>
2024-11-08 04:44:08 -0500

    sapien working with env variable hacks

--- pkgs/dev-shell/maniskill.nix
+++ pkgs/dev-shell/maniskill.nix
@@ -1,9 +1,16 @@
 { mkShell
 , python3
+, linuxPackages
+, vulkan-loader
 }:
 
 mkShell rec {
   name = "maniskill";
+
+  # TODO: fix these hacks
+  VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
+  __EGL_VENDOR_LIBRARY_FILENAMES = "/run/opengl-driver/share/glvnd/egl_vendor.d/10_nvidia.json";
+  SAPIEN_VULKAN_LIBRARY_PATH = "${vulkan-loader}/lib/libvulkan.so.1";
 
   packages = [
     (python3.withPackages (p: with p; [
@@ -12,4 +19,3 @@ mkShell rec {
     ]))
   ];
 }
-    
