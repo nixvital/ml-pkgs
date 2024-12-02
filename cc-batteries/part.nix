fc790c9c080a9fd1dc70ec822fcdb84412637793
Break Yang <breakds@gmail.com>
2024-12-02 15:10:45 -0800

    Recover scnlib 4.0.1

--- cc-batteries/part.nix
+++ cc-batteries/part.nix
@@ -4,11 +4,12 @@
   flake.overlays.cc-batteries = final: prev: {
     aria-csv-parser = final.callPackage ../cc-batteries/aria-csv-parser {};
     cpp-sort = final.callPackage ../cc-batteries/cpp-sort {};
+    scnlib = final.callPackage ../cc-batteries/scnlib {};
   };
 
   perSystem = {pkgs, lib, ...}: {
     packages = {
-      inherit (pkgs) aria-csv-parser cpp-sort;
+      inherit (pkgs) aria-csv-parser cpp-sort scnlib;
     };
   };
 }
