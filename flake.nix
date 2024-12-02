90891c63076885baed724105812fd1c3f91776c4
Break Yang <breakds@gmail.com>
2024-12-02 12:38:39 -0800

    Start building the parts

--- flake.nix
+++ flake.nix
@@ -2,170 +2,27 @@
   description = "Provide extra Nix packages for Machine Learning and Data Science";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    overlays = {
-      cc-batteries = import ./overlays/cc-batteries.nix;
-      bleeding = import ./overlays/bleeding.nix;
-      torch-family = import ./overlays/torch-family.nix;
-      jax-family = import ./overlays/jax-family.nix;
-      data-utils = import ./overlays/data-utils.nix;
-      simulators = import ./overlays/simulators.nix;
-      math = import ./overlays/math.nix;
-      misc = import ./overlays/misc.nix;
-      apis = import ./overlays/apis.nix;
-      symbolic = import ./overlays/symbolic.nix;
-      time-series = import ./overlays/time-series.nix;
-      tools = import ./overlays/tools.nix;
-      
-      # Default is a composition of all above.
-      default = nixpkgs.lib.composeManyExtensions [
-        self.overlays.cc-batteries
-        self.overlays.torch-family
-        self.overlays.jax-family
-        self.overlays.data-utils
-        self.overlays.simulators
-        self.overlays.math
-        self.overlays.misc
-        self.overlays.apis
-        self.overlays.symbolic
-        self.overlays.time-series
-        self.overlays.tools
-      ];
+  outputs = { self, flake-parts, ... }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
+    # Uncomment the following line to enable debug, e.g. in nix repl.
+    # See https://flake.parts/debug
+    
+    # debug = true;
+
+    systems = [
+      "x86_64-linux" "aarch64-linux" "aarch64-darwin"
+    ];
+
+    perSystem = { config, pkgs, ... }: {
+      formatter = pkgs.nixfmt-rfc-style;
     };
-  } // inputs.utils.lib.eachSystem [
-    "x86_64-linux"
-  ] (system:
-    let pkgs = import nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true;
-            cudaSupport = true;
-            # ChatGPT: When you compile a CUDA program, you can specify for
-            # which Compute Capability you want to build it. The resulting
-            # binary code will be optimized for that specific version of Compute
-            # Capability, and it won't run on GPUs with lower Compute
-            # Capability. If you want your CUDA program to be able to run on
-            # different GPUs, you should compile it for the lowest Compute
-            # Capability you intend to support.
-            #
-            # 7.5 - 20X0 (Ti), T4
-            # 8.0 - A100
-            # 8.6 - 30X0 (Ti)
-            # 8.9 - 40X0 (Ti)
-            cudaCapabilities = [ "7.5" "8.6" ];
-            cudaForwardCompat = true;
-          };
-          overlays = [
-            self.overlays.default
-          ];
-        };
-    in rec {
-      devShells = {
-        default = pkgs.callPackage ./pkgs/dev-shell {};
-        jax = pkgs.callPackage ./pkgs/dev-shell/jax.nix {};
-        quicktest = pkgs.callPackage ./pkgs/dev-shell/quicktest.nix {};
-        ddddocr = pkgs.callPackage ./pkgs/dev-shell/ddddocr.nix {};
-      };
-      # devShells.py38 = pkgs.callPackage ./pkgs/dev-shell {
-      #   python3 = pkgs.python38;
-      # };
 
-      packages = {
-        inherit (pkgs.python3Packages)
-          # ----- Torch Family -----
-          torchWithCuda
-
-          # ----- Jax Family -----
-          jaxlib
-          jax
-          equinox
-
-          # ----- Data Utils -----
-          awswrangler
-
-          # ----- Simulators -----
-          gym
-          gym3
-          atari-py-with-rom
-          ale-py-with-roms  # currently borken
-          procgen
-          highway-env
-          open3d
-          metadrive-simulator
-          robot-descriptions
-          mujoco-mjx
-          mujoco-menagerie
-          dm-control
-          python-fcl
-          sapien
-          glfw
-          gputil
-          aws-requests-auth
-          objaverse
-          objathor
-          ai2thor
-          procthor
-          robosuite
-          robocasa
-
-          # ----- Math -----
-          numpy-quaternion
-          chumpy
-          pulp
-
-          # ----- Tools -----
-          ddddocr
-          cos-python-sdk-v5
-          swankit
-          swanboard
-          swanlab
-          
-          # ----- Misc -----
-          numerapi
-
-          # ----- API -----
-          wolframalpha
-
-          # ----- Time Series -----
-          pyod
-          nfoursid
-          darts
-
-          # ----- Symbolic -----
-          pyjulia
-          pysr;
-
-        inherit (pkgs)
-          mujoco
-          cbc2
-          aria-csv-parser
-          cpp-sort
-          scnlib
-          unordered-dense
-          xxhash-cpp
-          vectorclass
-          outcome
-          quickcpplib
-          status-code
-          eve
-          aider-chat;
-      };
-
-      apps = {
-        # TODO(breakds): Deprecate this in the future
-        extract-langchain-deps = {
-          type = "app";
-          program = "${pkgs.callPackage ./pkgs/langchain/extract-langchain-deps.nix {}}/bin/extract";
-        };
-      };
-
-      checks =  {
-        full-devshell = self.devShells."${system}".default;
-      };
-    });
+    imports = [
+    ];
+  };
 }
