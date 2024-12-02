96586d8f94390766f924629e3ecec62b16420752
Break Yang <breakds@gmail.com>
2024-12-02 15:55:27 -0800

    Drop litellm, newer version in nixpkgs

--- overlays/apis.nix
+++ overlays/apis.nix
@@ -1,7 +1,6 @@
 final: prev: {
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     (python-final: python-prev: {
-      litellm = python-final.callPackage ../bleeding/litellm {};
       wolframalpha = python-final.callPackage ../pkgs/apis/wolframalpha {};
     })
   ];
