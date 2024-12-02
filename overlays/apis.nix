c35ea8edd36c27a671b84b106af14c30220718c0
Break Yang <breakds@gmail.com>
2024-12-02 15:56:19 -0800

    Clean up apis

--- overlays/apis.nix
+++
@@ -1,7 +0,0 @@
-final: prev: {
-  pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-    (python-final: python-prev: {
-      wolframalpha = python-final.callPackage ../pkgs/apis/wolframalpha {};
-    })
-  ];
-}
