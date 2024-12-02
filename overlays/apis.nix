6559b79078c3b4fce811fc9ea832077c8e526633
Break Yang <breakds@gmail.com>
2024-12-02 15:55:01 -0800

    Drop openai, newer version in nixpkgs

--- overlays/apis.nix
+++ overlays/apis.nix
@@ -1,7 +1,6 @@
 final: prev: {
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     (python-final: python-prev: {
-      openai = python-final.callPackage ../bleeding/openai {};
       litellm = python-final.callPackage ../bleeding/litellm {};
       wolframalpha = python-final.callPackage ../pkgs/apis/wolframalpha {};
     })
