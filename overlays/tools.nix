a20ddd39ab6e1a4f7b652315edc04bfa2c3970d5
Break Yang <breakds@gmail.com>
2024-12-02 16:00:30 -0800

    Drop aider-chat, already in nixpkgs

--- overlays/tools.nix
+++ overlays/tools.nix
@@ -3,10 +3,6 @@ final: prev: {
     (python-final: python-prev: {
       ddddocr = python-final.callPackage ../pkgs/ddddocr {};
       
-      # Dependencies for `aider`
-      tree-sitter-languages = python-final.callPackage ../bleeding/tree-sitter-language {};
-      grep-ast = python-final.callPackage ../pkgs/grep-ast {};
-      
       # SwanLab series
       cos-python-sdk-v5 = python-final.callPackage ../pkgs/cos-python-sdk-v5 {};
       swankit = python-final.callPackage ../pkgs/swanlab/swankit.nix {};
