36afffbeda5139ac9250e4780b1431e6f4a32124
Break Yang <breakds@gmail.com>
2024-12-07 16:13:14 -0800

    Updated ollama and its python library

--- gen-ai/part.nix
+++ gen-ai/part.nix
@@ -8,17 +8,27 @@
 
         magicattr = py-final.callPackage ./dspy/magicattr.nix { };
         dspy = py-final.callPackage ./dspy { };
+
+        ollama = py-final.callPackage ./ollama/python.nix {};
       })
     ];
+
+    ollama = final.callPackage ./ollama {};
   };
 
   perSystem = { pkgs, lib, ... }: {
-    packages = { inherit (pkgs.python3Packages) asyncer dspy; };
+    packages = {
+      inherit (pkgs) ollama;
+      inherit (pkgs.python3Packages) asyncer dspy;
+    };
 
     devShells.gen-ai = pkgs.mkShell {
       name = "gen-ai";
 
-      packages = [ (pkgs.python3.withPackages (p: with p; [ asyncer dspy ])) ];
+      packages = with pkgs; [
+        (python3.withPackages (p: with p; [ asyncer dspy ollama ]))
+        ollama
+      ];
     };
   };
 }
