13569cb030716ec71bc62ede3ebd5fdf3069998d
Break Yang <breakds@gmail.com>
2025-03-26 13:23:09 -0700

    Upgrade goose to 1.0.15

--- internal/part.nix
+++ internal/part.nix
@@ -11,6 +11,13 @@
     } (builtins.readFile ./pyproj2nix/pyproj2nix.py);
   };
 
+  flake.overlays.internal-hooks = final: prev: {
+    # TODO: Remove after upgraded to 25.04
+    writableTmpDirAsHomeHook = final.callPackage ({ makeSetupHook}: makeSetupHook {
+      name = "writeable-tmpdir-as-home-hook";
+    } ./hooks/writable-tmpdir-as-home.sh) {};
+  };
+
   perSystem = { system, pkgs-internal, lib, ... }: {
     _module.args.pkgs-internal = import inputs.nixpkgs {
       inherit system;
