c010d1bae77e7dca579c733509ea23f679fcefa7
Break Yang <breakds@gmail.com>
2025-05-26 08:49:09 -0700

    Upgrade temporal ui server

--- tools/temporal-ui-server/default.nix
+++ tools/temporal-ui-server/default.nix
@@ -1,7 +1,7 @@
 { lib, fetchFromGitHub, buildGoModule }:
 
 let pname = "temporal-ui-server";
-    version = "2.37.1";
+    version = "2.37.3";
 
 in buildGoModule {
   inherit pname version;
@@ -10,10 +10,10 @@ in buildGoModule {
     owner = "temporalio";
     repo = "ui-server";
     rev = "v${version}";
-    hash = "sha256-G5O5jsCnionQ3RREKPlfUKHeIoJSVxFI5CC2xtKGW58=";
+    hash = "sha256-FsW+5FIe7ouKreLh0gdb/s9ChaOkByWRHXjiWts4Gf0=";
   };
 
-  vendorHash = "sha256-WC+apki3BuLv/vLFJiF2MzCxiXfcFnSqo6JJyOnRYw4=";
+  vendorHash = "sha256-Skv+n0Da0Wgi8yjiHDcZsYwIWK4pbzdgsnrpurXudJ0=";
 
   meta = with lib; {
     description = "Temporal Web UI Go server (BFF for the SPA)";
