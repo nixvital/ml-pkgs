909d0cce8bfdb629c17f3231fdc7d590f66de808
Break Yang <breakds@gmail.com>
2025-07-16 09:44:25 -0700

    Package livekit-rtc with the fetched .so file

--- gen-ai/livekit/rtc.nix
+++ gen-ai/livekit/rtc.nix
@@ -1,16 +1,49 @@
 {
   lib,
+  stdenv,
   buildPythonPackage,
   fetchFromGitHub,
+  fetchurl,
+  autoPatchelfHook,
   setuptools,
+  unzip,
   requests,
   aiofiles,
   numpy,
   protobuf,
   types-protobuf,
+  system,
 }:
 
-buildPythonPackage rec {
+
+let
+  # See this link to decide the version
+  #  https://github.com/livekit/rust-sdks/blob/ab99f24eb4f55fe0c32a462faf1d74c752ffd76d/Cargo.toml
+  livekit-ffi-version = "0.12.28";
+
+  platform = {
+    "x86_64-linux" = "linux";
+    "aarch64-linux" = "linux";
+    "aarch64-darwin" = "macos";
+  }."${system}";
+  
+  arch = {
+    "x86_64-linux" = "x86_64";
+    "aarch64-linux" = "arm64";
+    "aarch64-darwin" = "arm64";
+  }."${system}";
+
+  livekit-ffi-hash = {
+    "x86_64-linux" = "sha256-QrnIkKpiBnERnrXv7sKi/W9iW1Co7KRxwiJIctTRQKE=";
+    "aarch64-darwin" = "1nh1z7mcyajk87z3gmdbdgrp8d1skd4q16b9dkykha3b7m0vib5x";
+  }."${system}";
+  
+  livekit-ffi = fetchurl {
+    url = "https://github.com/livekit/client-sdk-rust/releases/download/rust-sdks/livekit-ffi@${livekit-ffi-version}/ffi-${platform}-${arch}.zip";
+    hash = livekit-ffi-hash;
+  };
+
+in buildPythonPackage rec {
   pname = "livekit-rtc";
   version = "1.0.11";
   pyproject = true;
@@ -21,6 +54,25 @@ buildPythonPackage rec {
     tag = "rtc-v${version}";
     hash = "sha256-VHlAct3iRh6nA2xPmnEmUxiSF+l70FbcIJbhVLUVK/g=";
   };
+
+  # This package requires downloading the prebuilt rust-sdk binary of
+  # livekit-ffi and put it under resources directory. The `pyproject.toml` has a
+  # hook to copy it to the final build.
+  postPatch = ''
+    pushd livekit-rtc/livekit/rtc/resources
+    unzip "${livekit-ffi}"
+    ls .
+    popd
+  '';
+
+  nativeBuildInputs = [
+    unzip
+    autoPatchelfHook
+  ];
+
+  buildInputs = [
+    stdenv.cc.cc.lib
+  ];
 
   pypaBuildFlags = [ "livekit-rtc" ];
 
@@ -33,8 +85,6 @@ buildPythonPackage rec {
     protobuf
     types-protobuf
   ];
-
-  # doCheck = false; # no tests
 
   pythonImportsCheck = [ "livekit" ];
 
