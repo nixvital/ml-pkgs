c55c13c756bfb91bb722c18a9aea9af6cb978b99
Break Yang <breakds@gmail.com>
2025-10-10 18:34:21 -0700

    Update coding agents

--- gen-ai/claude-code/package.nix
+++ gen-ai/claude-code/package.nix
@@ -7,16 +7,16 @@
 
 buildNpmPackage rec {
   pname = "claude-code";
-  version = "2.0.8";
+  version = "2.0.14";
 
   nodejs = nodejs_20; # required for sandboxed Nix builds on Darwin
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-WxXkUZs/sQp7PJfSPCv8EwbvUGYgePhouKv/YFzOd14=";
+    hash = "sha256-U/wd00eva/qbSS4LpW1L7nmPW4dT9naffeMkHQ5xr5o=";
   };
 
-  npmDepsHash = "sha256-Z4JhcWw5Mn75ORtAV/hnhVgqIGVFMeEquXkcQxtacWA=";
+  npmDepsHash = "sha256-AwDDmDLd13+vfzUfLgbLL05RqgGdrlIvPJ5tBMYq2cU=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
