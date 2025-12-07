eddf8e2df6dba1e2c6bb895424c69ea89069cb4e
Break Yang <breakds@gmail.com>
2025-12-07 09:14:03 -0800

    Update codex and claude-code

--- gen-ai/claude-code/package.nix
+++ gen-ai/claude-code/package.nix
@@ -7,16 +7,16 @@
 
 buildNpmPackage rec {
   pname = "claude-code";
-  version = "2.0.51";
+  version = "2.0.60";
 
   nodejs = nodejs_20; # required for sandboxed Nix builds on Darwin
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-rfJZaACY+Kbm+0lWOPwAfl/x2yFxskLKZpJJhqlccSY=";
+    hash = "sha256-0ObgnvugfFSExnDDVhM6D99k21WnoWS4rtRm3Gb5JKw=";
   };
 
-  npmDepsHash = "sha256-BOdmR3pKp2oU+TuOyHRnfb/vvyXQaTdfdVidWB+4Ivo=";
+  npmDepsHash = "sha256-oyhcCgo1RKeQn+WrmyFWfE2k5+XXH+zHNG+LWj71VNU=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
